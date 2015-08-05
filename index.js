ef3fec68db8177a07ecfbb5df9a734ccd9731bc0
XVincentX <vincenz.chianese@yahoo.it>
2015-08-05 13:18:50 +0200

    index.js scheleton

---
+++ index.js
@@ -0,0 +1,8 @@
+import angular from 'angular'
+import formly from 'formly'
+  
+const ngModuleName = 'formlyMaterial'
+const ngModule = angular.module(ngModuleName, [formly])
+
+
+export default ngModuleName
