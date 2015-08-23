84b4998088093ea0b00db4e56464aa2f71e765f3
XVincentX <vincenz.chianese@yahoo.it>
2015-08-23 23:05:12 +0200

    Removed lodash dep

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-import _ from 'lodash'
-
-export default function mapSirenToFormly(fields) {
-  return _(this.action.fields).map(field => {
+export default function mapSirenToFormly(action) {
+  return action.fields.map(field => {
     return {
-      type: this.action.name + '.' + field.type,
+      type: action.name + '.' + field.type,
       key: field.name,
       defaultValue: field.value,
       templateOptions: {
         label: field.title || field.name
       }
     }
-  }).value()
+  })
 }
